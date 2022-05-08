437f7afa9430d70e308f93950295f14bced3d146
Dustin <dc.christianson@gmail.com>
2022-05-08 11:10:08 -0600

    added sleep time to start script to wait for servers to spin up and then added command to open firefox window at frontend dev server

--- start.sh
+++ start.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+# Open fresh terminal tab and start frontend dev server.
 echo "Starting front end dev environment..."
 
 gnome-terminal --tab --title FRONTEND -- npm run dev
 
+# Open fresh terminal tab and start backend dev server.
 echo "Starting back end dev environment..."
 
 gnome-terminal --tab --title BACKEND -- npx nodemon ./server/server.js
 
 echo "New tabs opened for FE and BE dev environments."
 
+# Open VS Code in project directory.
 echo "Starting VS Code in project directory..."
 
 code .
+
+# Sleep 1 second to wait for server startup.
+sleep 1
+
+# Open firefox on frontend dev server.
+echo "Starting firefox at http://localhost:3000..."
+
+firefox http://localhost:3000
 
 echo "Done."
